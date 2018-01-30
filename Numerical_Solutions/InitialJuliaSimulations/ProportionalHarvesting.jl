120ffa3405f0e6afd616956c39f622b32e8071f4
oscarram <oscar.ramirez@mathmods.eu>
2018-01-30 15:12:39 +0100

    New Updates

--- Numerical_Solutions/InitialJuliaSimulations/ProportionalHarvesting.jl
+++ Numerical_Solutions/InitialJuliaSimulations/ProportionalHarvesting.jl
@@ -3,13 +3,12 @@ using Plots; pyplot();
  
 r=0.8
 M=780500
-x0=M*0.75
-pr=0.74
-
+x0=M*0.5
+#pr=0.531176
+pr=0.631176
 f(u,p,t) = r*u*(1-u/M)-pr*r*u
 dt = 1//(2^(4))
-tspan = (0.0,10.0)
-
+tspan = (0.0,24.0)
 
 C1(x)=M
 C2(x)=0.0
@@ -17,4 +16,4 @@ C2(x)=0.0
 prob = ODEProblem(f,x0,tspan)
 sol = solve(prob,Tsit5(),reltol=1e-8,abstol=1e-8)
 Nfishes(t)=pr*sol(t)
-quadgk(Nfishes, 0, 10)
+quadgk(Nfishes, 0, 24.0)
