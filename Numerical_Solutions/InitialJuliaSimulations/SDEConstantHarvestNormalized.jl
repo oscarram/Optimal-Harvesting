9ce586740e2dede484bffdd05c9d07f9eac8693d
oscarram <oscar.ramirez@mathmods.eu>
2018-01-30 19:01:01 +0100

    NoiseTolerantDynamic

--- Numerical_Solutions/InitialJuliaSimulations/SDEConstantHarvestNormalized.jl
+++ Numerical_Solutions/InitialJuliaSimulations/SDEConstantHarvestNormalized.jl
@@ -3,8 +3,8 @@ using Plots; plotly(); # Using the Plotly backend
 
 ss=0.1;
 x0=1/2
-TH=6.3
-f(u,p,t) = u*(1-u)-1/4
+TH=6.5
+f(u,p,t) = u*(1-u)-0.9/4
 g(u,p,t) = ss*u
 dt = 1//(2^(4))
 tspan = (0.0,TH)
@@ -15,7 +15,7 @@ sol = solve(prob,SRIW1(),dt=dt)
 plot(sol, ylims=(0, 1))
 
 monte_prob = MonteCarloProblem(prob)
-sol = solve(monte_prob,num_monte=2300,paralle_type=:threads)
+sol = solve(monte_prob,num_monte=2000,paralle_type=:threads)
 summ = MonteCarloSummary(sol,0:0.01:TH)
 plot(summ,labels="Middle 95%", ylims=(0,1))
 summ = MonteCarloSummary(sol,0:0.01:TH; quantiles=[0.25,0.75])
